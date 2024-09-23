d865ad5aa8573721166eeee8082c8698b944638f
engrsakib <snazmussakib01@gmail.com>
2024-09-23 06:04:25 +0600

    faq addded

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}", './src/script/**/*.{html,js,jsx,ts,tsx}'],
+  content: ["./**/*.{html,js}", './src/script/**/*.{html,js,jsx,ts,tsx}', './pages/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
