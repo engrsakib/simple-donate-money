a8b2a2380cbfd0bb5b0b6f99c4beba908a54ca6c
engrsakib <snazmussakib01@gmail.com>
2024-09-24 00:23:12 +0600

    blog page added

--- pages/navigate.js
+++ pages/navigate.js
@@ -2,3 +2,6 @@ const homeP = document.getElementById('homeP');
 homeP.addEventListener('click', function(){
     window.location.href = '../index.html';
 });
+
+const blogTaka = document.getElementById("blog_taka");
+blogTaka.innerText = 17089;
